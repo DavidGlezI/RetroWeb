923d02c663675f02a09d86d1bd2521de7e0976cd
DavidGlezI <88803550+DavidGlezI@users.noreply.github.com>
2023-02-27 16:24:37 -0600

    ajax

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
 
 $(document).ready(function(){
-  let time = 10;
   let hiddenBox = $( ".reloj" );
 $( ".BotonFBI" ).on( "click", function(){
-  hiddenBox.html(time);
   hiddenBox.show();
   
 });
